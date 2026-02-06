f4ec2291532c84fc0ed042d5409c4946fae4f724
A. Salamntu <akhasalamntu@gmail.com>
2026-02-06 04:37:20 +0200

    Refactor PlaceOrderRequestDTO for cleaner code
    Removed unused fields and redundant getter/setter methods.

--- back-end/src/main/java/com/project/code/Model/PlaceOrderRequestDTO.java
+++ back-end/src/main/java/com/project/code/Model/PlaceOrderRequestDTO.java
@@ -1,72 +1,37 @@
 package com.project.code.Model;
 
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlaceOrderRequestDTO {
+
     private Long storeId;
+
     private String customerName;
     private String customerEmail;
     private String customerPhone;
-    private String datetime;
-    private List<PurchaseProductDTO> purchaseProduct;
+
     private Double totalPrice;
 
-    // Getters and Setters
+    private List<PurchaseProductDTO> purchaseProduct = new ArrayList<>();
 
-    public Long getStoreId() {
-        return storeId;
-    }
+    public PlaceOrderRequestDTO() {}
 
-    public void setStoreId(Long storeId) {
-        this.storeId = storeId;
-    }
+    public Long getStoreId() { return storeId; }
+    public void setStoreId(Long storeId) { this.storeId = storeId; }
 
-    public String getCustomerName() {
-        return customerName;
-    }
+    public String getCustomerName() { return customerName; }
+    public void setCustomerName(String customerName) { this.customerName = customerName; }
 
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
+    public String getCustomerEmail() { return customerEmail; }
+    public void setCustomerEmail(String customerEmail) { this.customerEmail = customerEmail; }
 
-    public String getCustomerEmail() {
-        return customerEmail;
-    }
+    public String getCustomerPhone() { return customerPhone; }
+    public void setCustomerPhone(String customerPhone) { this.customerPhone = customerPhone; }
 
-    public void setCustomerEmail(String customerEmail) {
-        this.customerEmail = customerEmail;
-    }
+    public Double getTotalPrice() { return totalPrice; }
+    public void setTotalPrice(Double totalPrice) { this.totalPrice = totalPrice; }
 
-    public String getCustomerPhone() {
-        return customerPhone;
-    }
-
-    public void setCustomerPhone(String customerPhone) {
-        this.customerPhone = customerPhone;
-    }
-
-    public String getDatetime() {
-        return datetime;
-    }
-
-    public void setDatetime(String datetime) {
-        this.datetime = datetime;
-    }
-
-    public List<PurchaseProductDTO> getPurchaseProduct() {
-        return purchaseProduct;
-    }
-
-    public void setPurchaseProduct(List<PurchaseProductDTO> purchaseProduct) {
-        this.purchaseProduct = purchaseProduct;
-    }
-
-    public Double getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(Double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
+    public List<PurchaseProductDTO> getPurchaseProduct() { return purchaseProduct; }
+    public void setPurchaseProduct(List<PurchaseProductDTO> purchaseProduct) { this.purchaseProduct = purchaseProduct; }
 }
