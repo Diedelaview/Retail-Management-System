581383b636c4556835ae1b98b52403cd0dcbf172
A. Salamntu <akhasalamntu@gmail.com>
2026-02-06 04:26:16 +0200

    Remove ComponentScan from CodeApplication
    Removed unnecessary ComponentScan annotation.

--- back-end/src/main/java/com/project/code/CodeApplication.java
+++ back-end/src/main/java/com/project/code/CodeApplication.java
@@ -2,15 +2,10 @@ package com.project.code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.project.code")
 public class CodeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CodeApplication.class, args);
-		
 	}
-
 }
